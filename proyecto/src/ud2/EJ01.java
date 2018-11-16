2a32bc2e33157c4e5510ee251c3b5970c0208d39
merchz <manuelenrique.ruiz.chimborazo@iestubalcain.net>
2018-11-16 20:45:35 +0100

    cambio de estructura del proyecto

--- proyecto/src/ud2/EJ01.java
+++ proyecto/src/ud2/EJ01.java
@@ -1,6 +1,6 @@
-package proyecto;
+package ud2;
 
-public class clase1 {
+public class EJ01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ proyecto/src/ud2/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author IFC
+ *
+ */
+package ud2;
